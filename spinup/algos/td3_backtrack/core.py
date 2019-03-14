32281128375aef2fc2b959766f4e2e9f1b2ce6c1
Philippe Weingertner <philippe.weingertner@gmail.com>
2019-03-14 21:44:21 +0100

    td3_backtrack algo, forgot to push core.py changes
    In the previous commit...

--- spinup/algos/td3_backtrack/core.py
+++ spinup/algos/td3_backtrack/core.py
@@ -20,6 +20,24 @@ def count_vars(scope):
     v = get_vars(scope)
     return sum([np.prod(var.shape.as_list()) for var in v])
 
+
+# ... start new for backtrack ...
+def get_vars(scope1='', scope2=''):
+    return [x for x in tf.trainable_variables() if scope1 in x.name and scope2 in x.name]
+
+def flat_concat(xs):
+    return tf.concat([tf.reshape(x,(-1,)) for x in xs], axis=0)
+
+def flat_grad(f, params):
+    return flat_concat(tf.gradients(xs=params, ys=f))
+
+def assign_params_from_flat(x, params):
+    flat_size = lambda p : int(np.prod(p.shape.as_list())) # the 'int' is important for scalars
+    splits = tf.split(x, [flat_size(p) for p in params])
+    new_params = [tf.reshape(p_new, p.shape) for p, p_new in zip(params, splits)]
+    return tf.group([tf.assign(p, p_new) for p, p_new in zip(params, new_params)])
+# ... end new for backtrack ...
+
 """
 Actor-Critics
 """
